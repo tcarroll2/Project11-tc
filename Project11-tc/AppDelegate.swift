51910ba9599292967b96661f5a62d0954aa10603
tcarroll2 <tc98207@gmail.com>
2020-04-25 15:13:47 -0500

    Initial Commit

---
+++ Project11-tc/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Project11-tc
+//
+//  Created by Thomas Carroll on 4/25/20.
+//  Copyright © 2020 Thomas Carroll. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
